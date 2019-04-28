f2b90bfbcd9b1eec39105294099bd51fa3e55b30
Szymon Kamiński <szy.kaminski@gmail.com>
2019-04-28 17:31:08 +0200

    init

---
+++ WPFUI_FOrganizer/ViewModels/ShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace WPFUI_FOrganizer.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public string ShellName { get; } = "File Organizer";
+        public string OrganizeEverything { get; } = "Organize everything";
+        public string OrganizeDocuments { get; } = "Documents";
+        public string OrganizePictures { get; } = "Pictures";
+        public string OrganizeMusic { get; } = "Music";
+        public string OrganizeVideos { get; } = "Videos";
+        public string OrganizeExesAndShortcuts { get; } = "Executables and Shortcuts";
+        public string OrganizeOther { get; } = "Other";
+        public string OrganizeFolders { get; } = "Folders";
+
+    }
+}
